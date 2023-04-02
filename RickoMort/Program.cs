5fcb9f8a170cbd3d07041c50654d6cbcdd02a804
mazylol <landonp95@gmail.com>
2023-04-01 23:05:42 -0400

    Basic Stuff

---
+++ RickoMort/Program.cs
@@ -0,0 +1,56 @@
+﻿using DotNetEnv;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RickoMort.GraphQL;
+
+namespace RickoMort
+{
+    internal abstract class Program
+    {
+        public static RickandMortyClient? Client;
+
+        static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            Env.TraversePath().Load();
+            var discordToken = Env.GetString("DEV_TOKEN");
+            var guildId = Convert.ToInt64(Env.GetString("GUILD_ID"));
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection
+                .AddRickandMortyClient()
+                .ConfigureHttpClient(client =>
+                    client.BaseAddress = new Uri("https://rickandmortyapi.com/graphql"));
+
+            IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+            Client = services.GetRequiredService<RickandMortyClient>();
+
+            var discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = discordToken,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged,
+                MinimumLogLevel = LogLevel.Debug
+            });
+
+            discord.Ready += DiscordReady;
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task DiscordReady(DiscordClient client, ReadyEventArgs e)
+        {
+            await client.UpdateStatusAsync(new DiscordActivity("Rick and Morty", ActivityType.Watching));
+        }
+    }
+}
